54f6d9469876cb12f602c061359162eeb15be02d
dipanshu914 <122882167+dipanshu914@users.noreply.github.com>
2025-04-09 15:37:07 +0530

    add unused setting in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,23 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: { globals: globals.browser },
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+    },
+    tseslint.configs.recommended,
+    // ⛔️ Disable specific TypeScript rule
+    {
+        files: ["**/*.ts"],
+        rules: {
+            "@typescript-eslint/no-unused-vars": "off",
+        },
+    },
 ]);
