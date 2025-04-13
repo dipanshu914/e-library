d4124269b9022784ee6c605314da8e7ad34a708e
dipanshu914 <122882167+dipanshu914@users.noreply.github.com>
2025-04-13 18:11:01 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -60,4 +60,65 @@ const createUser = async (
 }
 
 
-export { createUser };
+// Login Function Start Here
+const loginUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+
+    // 1st get the data.
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(createHttpError(400, "All fields are require"))
+    };
+
+    let user;
+    try {
+        // 2nd check the user is present in db or not.
+        user = await userModel.findOne({ email });
+        // validation.
+        if (!user) {
+            return next(createHttpError(404, "User not found"))
+        };
+
+    } catch (error) {
+        return next(createHttpError(500, "Error in db while fetching User data"))
+    };
+
+
+
+    // 3rd - Compare password
+    let isMatch
+    try {
+        isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) {
+            return next(createHttpError(400, "Username or password is incorrect"))
+        };
+
+    } catch (error) {
+        return next(createHttpError(500, "Error comparing password"))
+    };
+
+    //4th - password match create asscess token
+    try {
+        const token = sign({ sub: user._id }, config.jwtSecret as string, {
+            expiresIn: "7d"
+        });
+
+        // Send Response
+        res.status(200).json({
+            message: "OK",
+            accessToken: token
+        })
+
+    } catch (error) {
+        return next(createHttpError(500, "Error generating JWT token"))
+    }
+
+}
+
+
+export { createUser, loginUser };
